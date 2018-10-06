4c21e7d1833db70272996944d6105d162f72d25d
raffaella <ba.raffaella@yahoo.com>
2018-10-06 15:55:56 +0200

    aggiunto metodo crea utente

--- server/utenti/controller.js
+++ server/utenti/controller.js
@@ -1,11 +1,18 @@
 module.exports = (function(){
+
 	var getAllUsers = function(req, res){
 	res.send('tutti gli utenti');
-	}
+	};
+
+	var crea = function(req, res){
+		res.json(req.body);
+	};
+
 
 
 	return{
-		getAllUsers
+		getAllUsers,
+		crea
 
 	}
 
--- server/utenti/utenti.js
+++ server/utenti/utenti.js
@@ -1,8 +1,19 @@
 var express = require('express');
+
 var utenti = express.Router();
+
 var controller = require('./controller.js');
 
+//visualizza tutti gli utenti
 utenti.get('/', controller.getAllUsers);
+//crea un utente
+utenti.post('/', controller.crea);
+
+//visualizza un utente
+
+//modifica un utente
+
+//cancella un utente
 
 
 
